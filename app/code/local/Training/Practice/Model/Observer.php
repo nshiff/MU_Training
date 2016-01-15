c7d452a23e6a9dc8b4d16e932209e9dd18e3cef2
nshiff <nshiff@users.noreply.github.com>
2016-01-15 11:34:13 -0500

    decreasing product prices by . science.

--- app/code/local/Training/Practice/Model/Observer.php
+++ app/code/local/Training/Practice/Model/Observer.php
@@ -3,7 +3,8 @@
 class Training_Practice_Model_Observer{
 
 	public function catalogProductLoadAfter(Varien_Event_Observer $observer){
-
+		$product = $observer->getProduct();
+		$product->setSpecialPrice($product->getPrice() -1);
 	}
 
 }

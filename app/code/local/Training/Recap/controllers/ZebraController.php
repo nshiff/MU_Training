c70f4b8937ab6f47c9468c99365655c56bf4591f
nate <nshiff@users.noreply.github.com>
2016-02-12 14:52:18 -0500

    added custom frontname for 'recap' module. Also echoing class name of new model object for this module.

--- app/code/local/Training/Recap/controllers/ZebraController.php
+++ app/code/local/Training/Recap/controllers/ZebraController.php
@@ -3,15 +3,16 @@
 class Training_Recap_ZebraController extends Mage_Core_Controller_Front_Action{
 
     public function indexAction(){
-        $this->getResponse()->setBody('/ / / Hello I\'m a zebra / / /');
-    }
 
-    public function pageLayoutXmlAction(){
-        $this->loadLayout();
-        $this->getResponse()->setBody(
-            $this->getLayout()->getUpdate()->asString()
-        );
-        $this->getResponse()->setHeader('Content-Type', 'text/plain');
+        $model = Mage::getModel('training_recap/sample');
+
+        $body = '';
+        $body .= 'Now featuring:';
+        $body .= '<br>';
+        $body .= get_class($model);
+
+
+        $this->getResponse()->setBody($body);
     }
 
 

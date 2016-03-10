ae3134b0c72acd2f671da9da159733a9e65f83c6
nshiff <nshiff@users.noreply.github.com>
2016-03-10 18:22:42 -0500

    custom module generates htmk for block at /render/block/customBlock

---
+++ app/code/local/Training/Practice/Block/Example.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+class Training_Practice_Block_Example extends Mage_Core_Block_Abstract{
+
+	protected function _toHtml(){
+		return "HELLO WORLD";
+	}
+
+
+}
+
+
+
+
--- app/code/local/Training/Practice/controllers/BlockController.php
+++ app/code/local/Training/Practice/controllers/BlockController.php
@@ -2,18 +2,26 @@
 
 class Training_Practice_BlockController extends Mage_Core_Controller_Front_Action{
 
-    public function testAction(){
-	$message = 'you have reached the matrix';
-	$body = '';
-	$body .= '------------------------------------------------<br>';
-	$body .= '<span style="color:magenta">/ / / '	. $message . ' / / /</span><br>';	
-	$body .= '<span style="color:red">/ / / '	. $message . ' / / /</span><br>';
-	$body .= '<span style="color:yellow">/ / / '	. $message . ' / / /</span><br>';	
-	$body .= '<span style="color:lime">/ / / '	. $message . ' / / /</span><br>';
-	$body .= '<span style="color:blue">/ / / '	. $message . ' / / /</span><br>';
-	$body .= '------------------------------------------------<br>';
-        $this->getResponse()->setBody($body);
-    }
+	public function testAction(){
+		$message = 'you have reached the matrix';
+		$body = '';
+		$body .= '------------------------------------------------<br>';
+		$body .= '<span style="color:magenta">/ / / '	. $message . ' / / /</span><br>';	
+		$body .= '<span style="color:red">/ / / '	. $message . ' / / /</span><br>';
+		$body .= '<span style="color:yellow">/ / / '	. $message . ' / / /</span><br>';	
+		$body .= '<span style="color:lime">/ / / '	. $message . ' / / /</span><br>';
+		$body .= '<span style="color:blue">/ / / '	. $message . ' / / /</span><br>';
+		$body .= '------------------------------------------------<br>';
+		$this->getResponse()->setBody($body);
+	}
+
+	public function customBlockAction(){
+
+		$block = $this->getLayout()->createBlock('training_practice/example');
+		$this->getResponse()->setBody($block->toHtml());
+
+	}
+
 
 }
 

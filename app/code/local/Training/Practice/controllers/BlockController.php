ec88d7ad345b8d18584b60f6e52079065717dce1
nshiff <nshiff@users.noreply.github.com>
2016-03-10 17:55:11 -0500

    active route /render/block/test per Ex.1 Step 2

---
+++ app/code/local/Training/Practice/controllers/BlockController.php
@@ -0,0 +1,21 @@
+<?php
+
+class Training_Practice_BlockController extends Mage_Core_Controller_Front_Action{
+
+    public function testAction(){
+	$message = 'you have reached the matrix';
+	$body = '';
+	$body .= '------------------------------------------------<br>';
+	$body .= '<span style="color:magenta">/ / / '	. $message . ' / / /</span><br>';	
+	$body .= '<span style="color:red">/ / / '	. $message . ' / / /</span><br>';
+	$body .= '<span style="color:yellow">/ / / '	. $message . ' / / /</span><br>';	
+	$body .= '<span style="color:lime">/ / / '	. $message . ' / / /</span><br>';
+	$body .= '<span style="color:blue">/ / / '	. $message . ' / / /</span><br>';
+	$body .= '------------------------------------------------<br>';
+        $this->getResponse()->setBody($body);
+    }
+
+}
+
+
+

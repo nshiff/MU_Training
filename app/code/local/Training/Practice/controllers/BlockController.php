cb9c3ae869107f56325b2dd6692f9549895afa95
nshiff <nshiff@users.noreply.github.com>
2016-03-10 19:06:01 -0500

    using the global Mage registry to share values between a module's controller and its templates

--- app/code/local/Training/Practice/controllers/BlockController.php
+++ app/code/local/Training/Practice/controllers/BlockController.php
@@ -2,7 +2,7 @@
 
 class Training_Practice_BlockController extends Mage_Core_Controller_Front_Action{
 
-	public function testAction(){
+	public function testAction(){								// /training/practice/test
 		$message = 'you have reached the matrix';
 		$body = '';
 		$body .= '------------------------------------------------<br>';
@@ -15,19 +15,26 @@ class Training_Practice_BlockController extends Mage_Core_Controller_Front_Actio
 		$this->getResponse()->setBody($body);
 	}
 
-	public function customBlockAction(){
+	public function customBlockAction(){						// /training/practice/custom
 
 		$block = $this->getLayout()->createBlock('training_practice/example');
 		$this->getResponse()->setBody($block->toHtml());
 
 	}
 
-	public function templateBlockAction(){
+	public function templateBlockAction(){						// /training/practice/templateBlock
 		$block = $this->getLayout()->createBlock('core/template');
 		$block->setTemplate('training/practice/example.phtml');
 		$this->getResponse()->setBody($block->toHtml());;
 	}
 
+	public function registryAction(){
+		Mage::register('FORTRESS_LOCATION','In the Forbidden Forest');
+		$block = $this->getLayout()
+		->createBlock('training_practice/registry')
+		->setTemplate('training/practice/registry.phtml');
+		$this->getResponse()->setBody($block->toHtml());
+	}
 }
 
 

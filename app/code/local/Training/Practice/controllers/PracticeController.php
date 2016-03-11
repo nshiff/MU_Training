10a6efc796739709ae20124cc9f3e4cbcff9384c
nshiff <nshiff@users.noreply.github.com>
2016-03-11 12:53:56 -0500

    Ex.1 Step 0? conforming to convention of using the module's 'class group' as its front name. this is arbitrary and confusing because names need to match in many places. this change attempts to aid completion of Step 1.

---
+++ app/code/local/Training/Practice/controllers/PracticeController.php
@@ -0,0 +1,35 @@
+<?php
+
+class Training_Practice_PracticeController extends Mage_Core_Controller_Front_Action{
+
+	// This class defines handlers for this module's routes:
+	// http://homepage.com/[This Module's Frontname]/practice
+	// the subpath 'practice' is arbitrary
+	// but by convention it follow the module's name.
+
+	public function indexAction(){									// /practice
+		$body = '';
+		$body .= '-------------------------------------<br>';
+		$body .= ' * * * Things are working * * *<br>';
+		$body .= '-------------------------------------<br>';	
+		$this->getResponse()->setBody($body);
+	}
+
+	public function sweetAction(){									// /practice/sweet
+		$body = '';
+		$body .= '-------------------------------------<br>';
+		$body .= ' * * * Things are Sweet * * *<br>';
+		$body .= '-------------------------------------<br>';	
+		$this->getResponse()->setBody($body);
+	}
+
+	public function reallysweetAction(){							// /practice/reallysweet
+		$body = '';
+		$body .= '-------------------------------------<br>';
+		$body .= ' * * * Things are Really Sweet * * *<br>';
+		$body .= '-------------------------------------<br>';	
+		$this->getResponse()->setBody($body);
+	}
+
+}
+

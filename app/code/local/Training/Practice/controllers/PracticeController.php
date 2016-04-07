04c98d2f82dcf111350e6c6bea4e9482dab8c17c
nshiff <nshiff@users.noreply.github.com>
2016-04-07 16:59:11 -0400

    Ex.2 Step1f. enumerating available categories for each store

--- app/code/local/Training/Practice/controllers/PracticeController.php
+++ app/code/local/Training/Practice/controllers/PracticeController.php
@@ -15,31 +15,7 @@ class Training_Practice_PracticeController extends Mage_Core_Controller_Front_Ac
 		$this->getResponse()->setBody($body);
 	}
 
-	protected function _displayChildCategories($category){
-		
-		$resource = $category->getResource();
-		
-		if( Mage::helper('catalog/category_flat')->isEnabled() ){
-			$table = $resource->getMainTable();
-		}
-		else{
-			$table = $resource->getEntityTable();
-		}
-		
-		// Retrieve the read adapter to work with the select object
-		$select = $resource->getReadConnection()->select()
-			->from($table, '*')
-			->where('parent_id=?', $category->getId());
-		
-		
-		$this->getResponse()->appendBody("cool" . "\n");
-		
-		
-	}
-	
-	
-	
-	public function rootCategoryListAction(){
+	public function rootCategoryListAction(){							// /practice/rootCategoryList
 		$stores = Mage::getResourceModel('core/store_collection');
 		$rootIds = $stores->walk('getRootCategoryId');
 		var_dump($rootIds);
@@ -58,7 +34,62 @@ class Training_Practice_PracticeController extends Mage_Core_Controller_Front_Ac
 			$this->_displayChildCategories($category);
 		}
 	}
-	
-	
+
+	protected function _displayChildCategories($category){
+
+		$resource = $category->getResource();
+
+		// $table name logic
+		if( Mage::helper('catalog/category_flat')->isEnabled() ){
+			$table = $resource->getMainTable();
+		}
+		else{
+			$table = $resource->getEntityTable();
+		}
+
+		// Retrieve the read adapter to work with the select object
+		$select = $resource->getReadConnection()->select()
+			->from($table, '*')
+			->where('parent_id=?', $category->getId());
+
+		// conditional "name" logic
+		if( ! Mage::helper('catalog/category_flat')->isEnabled()){
+			$attribute = $resource->getAttribute('name');
+
+			$attributeTable = $attribute->getBackend()->getTable();
+
+			$select->joinInner(
+				array($attributeTable),
+				"{$table}.entity_id={$attributeTable}.entity_id " .
+					"AND attribute_id={$attribute->getId()}",
+				array('name' => 'value')
+			);
+		}
+
+
+		$rows = $resource->getReadConnection()->fetchAll($select);
+
+		foreach ($rows as $row){
+
+			$childCat = Mage::getModel('catalog/category')->setData($row);
+
+			$output = str_pad("", $childCat->getLevel() * 2, " ") .
+				"{$childCat->getId()} {$childCat->getName()}\n";
+			$this->getResponse()->appendBody($output);
+
+
+
+		}
+
+
+
+
+
+
+
+	}
+
+
+
 }
 
